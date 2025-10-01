c780b0228b740e5de20e733883fabf34e6148b5f
felipebuso <felipe.buso@hotmail.com>
2025-09-30 22:00:04 -0300

    feat: vercel config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/classify-text": {
-        target: "http://localhost:8000/api",
-        changeOrigin: true,
-      },
-      "/classify-file": {
-        target: "http://localhost:8000/api",
-        changeOrigin: true,
+export default ({ mode }: { mode: string }) => {
+  // Carrega as variáveis de ambiente do .env.<mode>
+  const env = loadEnv(mode, process.cwd(), "");
+  const API_URL = env.VITE_API_URL || "http://localhost:8000/api";
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_PORT) || 3000,
+      proxy: {
+        "/classify-text": {
+          target: API_URL,
+          changeOrigin: true,
+        },
+        "/classify-file": {
+          target: API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  });
+};
